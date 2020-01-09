d3e40d7b8ca3e3c26f8e06b5f0d360e1dec98638
niall moore <moore.nialls@gmail.com>
2020-01-09 13:53:24 +0000

    added images and made changes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// testimonial slider
-
 const sliderContainer=document.querySelector(".test-slider");
 const slides=sliderContainer.children;
 const containerWidth=sliderContainer.offsetWidth;
@@ -14,17 +12,17 @@ const responsive=[
 ]
 
 function load(){
-    for(Let i=0; i<responsive.length; i++)
+    for(let i=0; i<responsive.length; i++)
         if(window.innerWidth>responsive[i].breakPoint.width){
-
+          itemPerSlide=responsive[i].breakPoint.item;      
         }
-    
-    Start();
+   
+    start();
 }
 
 function start(){
     totalWidth=0;
-    for(Let i=0; i<slides.length; i++){
+    for(let i=0; i<slides.length; i++){
         slides[i].style.width=(containerWidth/itemPerSlide)-margin + "px";
         slides[i].style.margin=margin/2 + "px";
         totalWidth+=containerWidth/itemPerSlide;
@@ -34,7 +32,7 @@ function start(){
      slideDots=Math.ceil(slides.length/itemPerSlide);
     //console.log(slideDots)
 
-    for(Let i=0; i<slideDots; i++){
+    for(let i=0; i<slideDots; i++){
         const div=document.createElement("div");
         div.id=i;
         div.setAttribute("onclick","controlSlide(this)");
@@ -45,8 +43,8 @@ function start(){
     }
 }
 
-Let currentSlide=0;
-Let autoSlide=0;
+let currentSlide=0;
+let autoSlide=0;
 
 function controlSlide(element){
     clearInterval(timer)
@@ -58,24 +56,24 @@ function controlSlide(element){
 
 function changeSlide(currentSlide){
     controlButtons=document.querySelector(".slide-controls").children;
-        
-    for(Let i=0; i<controlButtons.length; i++){        
+       
+    for(let i=0; i<controlButtons.length; i++){        
         controlButtons[i].classList.remove("active")
     }
     //console.log(currentSlide)            
-    controlButtons[currentslide].classList.add("active")
+    controlButtons[currentSlide].classList.add("active")
 
     sliderContainer.style.marginLeft=-(containerWidth*currentSlide) + "px";
 }
 
-//autoplay 
+//autoplay
 
 function autoPlay(){
     //change between line 74 and 75
     //console.log("call")
     console.log(slideDots-1)
     if(autoSlide==slideDots-1){
-        autoSlide=0; 
+        autoSlide=0;
     }
     else{
         autoSlide++;
@@ -83,5 +81,6 @@ function autoPlay(){
     changeSlide(autoSlide)
 }
 
-Let timer=setInterval(autoPlay,5000),
+let timer=setInterval(autoPlay,5000)
 window.onload=load();
+
